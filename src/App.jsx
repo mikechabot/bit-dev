2c85ef2fc4ade3ed95740ff34657a04e60472397
Mike Chabot <mchabot@draftkings.com>
2020-10-30 16:26:33 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+import ProductList from '@bit/mikechabot.collection.product-list';
+
+function App() {
+  return (
+      <div className="App">
+        <ProductList />
+      </div>
+  );
+}
+
+export default App;
